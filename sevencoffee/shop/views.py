d774279612339f95f363dba6c119253a1d5db3f4
ledgey <craig.ledgerwood81@gmail.com>
2021-03-16 20:34:56 +0000

    changed homescreen

--- sevencoffee/shop/views.py
+++ sevencoffee/shop/views.py
@@ -27,11 +27,7 @@ for i in range(1, 101):
     (hw, d, l) = (random.uniform(0, 2), random.uniform(0, 20), random.uniform(0,10))
     css.append(csselement % (i, hw, hw, d, l))
 
-# with open('/Users/craigledgerwood/Documents/seven/sevencoffee/shop/static/shop/dripstyle.css', 'a+') as f:
-#     f.write(css)
-#     f.close()
 
-# print(f.filename)
 
 
 class Index(View):
@@ -45,8 +41,5 @@ class Coming(View):
 
 class Home(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'test.html', {'css_string_list': css})
+        return render(request, 'home.html', {'css_string_list': css})
 
-class HomeTest(View):
-    def get(self, request, *args, **kwargs):
-        return render(request,template_name='home4.html')
